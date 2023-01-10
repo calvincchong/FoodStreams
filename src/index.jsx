0311c452563153fa69f58f2c41642c8e501fa96a
Calvin Chong <calvin.chong184@gmail.com>
2023-01-09 19:53:42 -0500

    Changed <App /> component to reference App component in other files. git status

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 // Import from react-dom the ability to create a root render
 import { createRoot } from "react-dom/client";
+import App from './components/App.jsx';
 // create the root of the app by selection where the app should be mounted in the dom
 const root = createRoot(document.getElementById("root"));
 
 // Here is out base App component.
 // Notice we are NOT using jsx here. This is because we have not set up babel yet.
-const App = React.createElement("h1",null,"Hello World");
+// const App = React.createElement("h1",null,"Hello World");
 
 // render the root element with the provided component
 root.render(<App />);
