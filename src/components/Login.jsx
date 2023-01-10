ad5d3b3459eff339f9fa5bfb8f9a03a5328db647
Calvin Chong <calvin.chong184@gmail.com>
2023-01-10 10:54:46 -0500

    Added boilerplate component for Login Component

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
+
+const Login = ({props}) => {
+
+  return (
+    <div>
+      <h2>Login Component</h2>
+      A centered form belongs here.
+    </div>
+  );
+}
+
+export default Login;
