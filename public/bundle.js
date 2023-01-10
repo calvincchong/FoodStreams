0577c23cc622df5f9d0172a4e726c5f06dc63a13
Calvin Chong <calvin.chong184@gmail.com>
2023-01-09 20:15:12 -0500

    Adding bundle.js and yarn.lock files before pull

--- public/bundle.js
+++ public/bundle.js
@@ -21,12 +21,12 @@ throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index
 
 /******/ 	});
 /************************************************************************/
-/******/ 	
+/******/
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.jsx"]();
-/******/ 	
+/******/
 /******/ })()
 ;
