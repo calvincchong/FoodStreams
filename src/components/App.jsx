a6cb3cb3f6ff01b94a8aa51974eb7d7b8146dec1
Calvin Chong <calvin.chong184@gmail.com>
2023-01-09 20:13:07 -0500

    Got front end to commit properly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
-import { React, useState, useEffect } from 'react';
 
 // components
 
-export const App = () => {
-  const[isConnected, setIsConnected] = useState(false);
+const App = ({props}) => {
+  const [isConnected, setIsConnected] = useState(false);
 
   return (
     <div>
       <h1> Restaurant Orders</h1>
       It is {isConnected} that we are connected.
     </div>
-  )
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 // Import from react-dom the ability to create a root render
 import { createRoot } from "react-dom/client";
-import App from './components/App.jsx';
+import App from "./components/App.jsx";
 // create the root of the app by selection where the app should be mounted in the dom
 const root = createRoot(document.getElementById("root"));
 
