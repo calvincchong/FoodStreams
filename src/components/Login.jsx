2de8623c77df2833cfc44d2cd3c535dce2b20167
Calvin Chong <calvin@lenslockers.com>
2023-01-10 17:02:55 -0500

    Added singular form componen without CSS Styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import io from 'socket.io-client';
 import axios from 'axios';
 
 const Login = ({props}) => {
+  const [username, setUserName] = useState('');
+  const navigate = useNavigate();
+
+
 
   return (
     <div>
-      <p className="text-2xl font-bold underline">Login Component </p>
+      <p className="login_container text-2xl font-bold underline">Login Component </p>
       <p className="text-xl"> Login component is displayed at url '/';</p>
-      A centered form belongs here.
-      TODO: Determine how quickly I can set up login or not login yes
-      Live testing currently works:
-      Some issue with the task breakdown
+      <form className="login_form">
+        <label> Please enter your username </label>
+        <input
+           type='text'
+           name='username'
+           id='username'
+           required
+           onChange={e => setUserName(e.target.value)}
+           value={username}
+        />
+        <button>Sign In</button>
+      </form>
     </div>
   );
 }
