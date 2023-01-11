805411f556655d70c77cc180642e451362ec3d81
Calvin Chong <calvin@lenslockers.com>
2023-01-10 20:49:32 -0500

    Added rough equally sized columns onto the page with middle one having a border

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -29,17 +29,36 @@ const Board = ({props}) => {
       <div className="board-header">This is on top</div>
       <h2> Order Board</h2>
       <div>It is {isConnected} that we are connected.</div>
-      <div className="board-canvas flex flex-row">
-        <div className="ko-list listwrapper flex-col basis-1/3">
-          <h2>Queue</h2>
+
+      <div className="board-canvas flex flex-row min-h-8/10">
+
+        <div className='pending-wrapper basis-1/3'>
+          <h3>Pending</h3>
+          <div className='ko-list flex-col  min-h-8/10'>
+            <div className='listWrapper'>
+            </div>
+          </div>
         </div>
-        <div className="ko-list listwrapper flex-col basis-1/3">
+
+        <div className='working-wrapper flex-col min-h-8/10 basis-1/3'>
           <h2>Working On</h2>
+          <div className="working-card-container border-2  border-sky-500 min-h-full">
+            <div className="working-cards  min-h-full">
+              TEST CONTENT
+            </div>
+          </div>
         </div>
-        <div className="ko-list listwrapper flex-col basis-1/3">
-          <h2>Completed</h2>
+
+        <div className='working-wrapper basis-1/3'>
+          <div className="ko-list flex-col basis-1/3 min-h-8/10">
+            <div className="listWrapper border ">
+              <h2>Completed</h2>
+            </div>
+          </div>
         </div>
+
       </div>
+
     </div>
   );
 }
