a08f59d06f61a0aeca6347cab42a178cc5db3fbc
Calvin Chong <calvin@lenslockers.com>
2023-01-12 13:22:07 -0500

    Deleted Unused Component Files

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 
 const Item = ({item}) => {
   return(
-    <div className='border border-gray-200 border-sky-500 ml-2'>
+    <div className='border border-gray-200 border-sky-500 mx-4 px-2'>
       <label>
         <input type='checkbox' />
          {item}
